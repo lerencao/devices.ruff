7b7d33e1bf6b8de19f5f9607b0f4a3ce526cee5c
Cao Jiafeng <funfriendcjf@gmail.com>
2016-05-07 16:07:09 +0800

    led change colors

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ $.ready(function (error) {
         return;
     }
 
-    $('#led-r').turnOn();
+    var led = $('#led');
+    setInterval(function() {
+        led.turnOn();
+
+        var r = Math.random() * 255;
+        var g = Math.random() * 255;
+        var b = Math.random() * 255;
+        led.setRGB(r, g, b);
+        console.log("led should go on");
+    }, 200);
 });
 
 $.end(function () {
-    $('#led-r').turnOff();
+    $('#led').turnOff();
 });
--- test/led_test.js
+++ test/led_test.js
@@ -6,17 +6,17 @@ var appRunner = require('ruff-app-runner');
 var mock = require('ruff-mock');
 
 var verify = mock.verify;
-
+var atLeast = mock.atLeast;
 var appPath = path.join(__dirname, '..');
 
 module.exports = {
     'test should run application': function () {
         appRunner
             .run(appPath, function () {
-                verify($('#led-r')).turnOn();
+                verify($('#led'), atLeast()).turnOn();
             })
             .end(function () {
-                verify($('#led-r')).turnOff();
+                verify($('#led', atLeast())).turnOff();
             });
     }
 };
